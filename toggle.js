5d38f2780d02c5fad07f41a1cc71f21a3643300f
kelvinirungu <kelvins.irungu@gmail.com>
2022-07-02 14:06:30 +0300

    changes to category styling

---
+++ toggle.js
@@ -0,0 +1,14 @@
+// const card = document.querySelector(".card");
+// const conta = document.querySelector(".conta");
+
+
+//  conta.addEventListener("mousemove", (e) => {
+//   let xAxis = (window.innerWidth / 2 - e.pageX) /25;
+//   let yAxis = (window.innerheight / 2 - e.pageY) /25;
+//   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+//  });
+
+//  conta.addEventListener()
+
+
+
